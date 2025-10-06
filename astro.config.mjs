87e642441939588bd7430cf5abd77bcccfa2d408
Dev Generico IG <dev.generico2@gmail.com>
2025-10-06 10:32:49 -0600

    Removi unos comentarios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://arturozvezda.github.io/profile/',
-  base: '/profile/',
+  site: "https://arturozvezda.github.io/profile/",
+  base: "/profile/",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
